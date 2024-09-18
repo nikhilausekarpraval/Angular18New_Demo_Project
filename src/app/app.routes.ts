34007a8a3533264e95a2141870a23bd7b55b7298
Nikhil Ausekar <nikhil.a@pravaltech.com>
2024-09-18 13:11:32 +0530

    project setup done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    // {
+    //     component:EmployeeDashboardComponent,
+    //     path:"/employee"
+    // },{
+    //     component:TaskDashboardComponent,
+    //     path:"/task"
+    // }
+];
