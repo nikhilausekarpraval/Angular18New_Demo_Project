62b00c4b6c96ca248e70ad4601c3c3fe96fa4045
Nikhil Ausekar <nikhil.a@pravaltech.com>
2024-09-18 13:13:15 +0530

    added constants

---
+++ src/app/Constants/Constatns.ts
@@ -0,0 +1,2 @@
+export const EMPLOYEE_TABLE_HEADERS =["Id","Name","Position","Designation","Email","Mobile. No.","Created On Date","Created By"];
+export const TASK_TABLE_HEADERS =["Id","Name","Description","Assigned On Date","End Date","Created On Date","Created By"];
---
+++ src/app/Interfaces/interfaces.ts
@@ -0,0 +1,14 @@
+export interface IEmployee{
+          id:number;
+          name:string;
+          position:string;
+          designation:string;
+          email:string;
+          mobileNo:string;
+          createdOnDt:Date;
+          createdBy:string;
+}
+
+export interface ITask{
+
+}
